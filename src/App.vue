1d5adb9d5d6761f199aab69988011f427b152c7b
Aleksey Sereda <sereddos@gmail.com>
2020-04-23 08:44:59 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PostForm/>
+    <strong>Posts count: {{postsCount}}</strong>
+    <div class="post" v-for="post in validPost" :key="post.id">
+      <h4 class="post__title">{{post.title}}</h4>
+      <p class="post__body">{{post.body}}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {mapGetters, mapActions} from 'vuex'
+import PostForm from './components/PostForm'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  // computed: {
+  //   getAllPosts() {
+  //     return this.$store.getters.allPosts
+  //   }
+  // },
+  components: { PostForm },
+  computed: mapGetters(['validPost', 'postsCount']),
+  methods: mapActions(['fetchPosts']),
+  async mounted() {
+    // this.$store.dispatch('fetchPosts')
+    this.fetchPosts(4)
   }
 }
 </script>
@@ -21,8 +35,25 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.post {
+  margin-bottom: 40px;
+}
+
+.post__title {
+  padding: 0;
+  margin-bottom: 5px;
+}
+
+.post__title:first-letter {
+  text-transform: uppercase;
+}
+
+.post__body {
+  margin: 0;
+}
+
 </style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <form @submit.prevent="submit">
+    <input type="text" placeholder="title" v-model="title">
+    <input type="text" placeholder="body" v-model="body">
+    <button>Create post</button>
+  </form>
+</template>
+
+<script>
+import {mapMutations} from 'vuex'
+export default {
+  data() {
+    return {
+      title: '',
+      body: ''
+    }
+  },
+  methods: {
+    ...mapMutations(['createPost']),
+    submit() {
+      this.createPost({
+        title: this.title,
+        body: this.body,
+        id: Date.now()
+      })
+      this.title = this.body = ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+input {
+  display: block;
+  width: 200px;
+  margin-bottom: 10px;
+}
+</style>
